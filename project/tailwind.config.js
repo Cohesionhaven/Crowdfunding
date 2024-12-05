0d2297f8e97f8279b2386e2219412121feb9fad6
CohesionHavne <cohesionhaven@gmail.com>
2024-12-05 20:12:28 +0530

    Update tailwind.config.js

--- project/tailwind.config.js
+++ project/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from "tailwindcss-animate";
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
@@ -19,5 +21,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 };
